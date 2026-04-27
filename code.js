1408fd9303fba3bac680bbe045b4d01ef0b2ba7c
daniel luke <manthid5@gmail.com>
2026-04-27 14:42:45 +0300

    edit code in code.js

--- code.js
+++ code.js
@@ -1,3 +1,9 @@
 const header = document.querySelector("header");
 
 header.style.backgroundColor = "blue";
+
+const fullName = document.querySelector("#full-name").value;
+
+const email = document.querySelector("#email").value;
+
+const message = document.querySelector("#message").value;
